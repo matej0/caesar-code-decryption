56c42bb61b080fa5b0adf1694ba4315e94a6e2b9
Matej B <80791000+matej0@users.noreply.github.com>
2022-01-18 12:33:30 +0100

    Add files via upload

---
+++ cesar.py
@@ -0,0 +1,16 @@
+def main():
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    encrypted = str(input("xd: ")).strip() ##remove spaces
+    offset = 0
+    for i in range(len(alphabet)):
+        decrypted = ""
+        for j in encrypted:
+            if (j in alphabet): #python easy
+                offset = alphabet.find(j)
+                decrypted += alphabet[(offset - i) % len(alphabet)]
+            else:
+                decrypted += j
+        print("key %d: %s" % (i, decrypted))
+
+if __name__ == "__main__":
+     main()
